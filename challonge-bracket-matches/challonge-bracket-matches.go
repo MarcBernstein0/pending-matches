22fac018dd5ee322602855e267149eb22a0bc042
Marc Bernstein <marcbernstein98@gmail.com>
2023-11-17 01:09:53 -0500

    basic fetchTournaments and unit test working

--- challonge-bracket-matches/challonge-bracket-matches.go
+++ challonge-bracket-matches/challonge-bracket-matches.go
@@ -2,11 +2,13 @@ package challongebracketmatches
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
 	"net/http"
 	"time"
+
+	"github.com/MarcBernstein0/pending-matches/models"
 )
 
 var (
@@ -26,7 +28,7 @@ type (
 	FetchData interface {
 		// FetchTournaments fetch all tournaments created after a specific date
 		// GET https://api.challonge.com/v2.1/tournaments.json?page=1&per_page=25
-		FetchTournaments(ctx context.Context, date string) (map[int]string, error)
+		FetchTournaments(ctx context.Context, date string) (map[string]string, error)
 	}
 )
 
@@ -40,7 +42,10 @@ func New(baseURL, apiKey string, client *http.Client, contextTimeout time.Durati
 }
 
 // Return map of type int -> string where int is the tournamentId and string is the game name
-func (c *customClient) FetchTournaments(ctx context.Context, date string) (map[int]string, error) {
+func (c *customClient) FetchTournaments(ctx context.Context, date string) (map[string]string, error) {
+
+	resMap := make(map[string]string)
+
 	params := map[string]string{
 		"state":         "in_progress",
 		"created_after": date,
@@ -50,6 +55,7 @@ func (c *customClient) FetchTournaments(ctx context.Context, date string) (map[i
 	defer cancelCtx()
 
 	requestURL := c.baseURL + "/tournaments.json"
+	fmt.Println(requestURL)
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, requestURL, nil)
 	if err != nil {
 		return nil, err
@@ -57,24 +63,29 @@ func (c *customClient) FetchTournaments(ctx context.Context, date string) (map[i
 
 	res, err := c.get(req, params)
 	if err != nil {
+		fmt.Println(err)
 		return nil, err
 	}
 
 	if res.StatusCode != http.StatusOK {
-		fmt.Println(err)
 		return nil, fmt.Errorf("%w. %s", ErrResponseNotOK, http.StatusText(res.StatusCode))
 	}
+	fmt.Println(res)
 
 	defer res.Body.Close()
 
-	body, err := io.ReadAll(res.Body)
+	var tournaments models.Tournaments
+	err = json.NewDecoder(res.Body).Decode(&tournaments)
 	if err != nil {
 		fmt.Println(err)
-		return nil, err
+		return nil, fmt.Errorf("%w. %s", err, http.StatusText(http.StatusInternalServerError))
 	}
-	fmt.Println(string(body))
+	fmt.Printf("tournaments value %+v\n", tournaments)
+	for _, tournament := range tournaments.Data {
+		resMap[tournament.Id] = tournament.Attributes.GameName
+	}
 
-	return nil, nil
+	return resMap, nil
 }
 
 func (c *customClient) get(req *http.Request, params map[string]string) (resp *http.Response, err error) {
@@ -88,9 +99,6 @@ func (c *customClient) get(req *http.Request, params map[string]string) (resp *h
 		q.Add(key, val)
 	}
 	req.URL.RawQuery = q.Encode()
-	fmt.Println(req.URL)
-	fmt.Println(req.Header)
-	fmt.Println(req)
 
 	return c.client.Do(req)
 }
--- challonge-bracket-matches/challonge-bracket-matches_test.go
+++ challonge-bracket-matches/challonge-bracket-matches_test.go
@@ -14,12 +14,17 @@ import (
 
 var server *httptest.Server
 
+const MOCK_API_KEY = "mock api key"
+
 func TestMain(m *testing.M) {
 	fmt.Println("Mock Server")
 	server = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		trimPath := strings.TrimSpace(r.URL.Path)
+		fmt.Println("trim path", trimPath)
 		switch trimPath {
+		case "/tournaments.json":
+			mockFetchTournamentEndpoint(w, r)
 		default:
 			http.NotFoundHandler().ServeHTTP(w, r)
 		}
@@ -31,7 +36,7 @@ func TestMain(m *testing.M) {
 
 func TestCreateCustomClient(t *testing.T) {
 	// Given
-	givenCustomClient := customClient{
+	givenCustomClient := &customClient{
 		baseURL:        "testEndpoint",
 		client:         http.DefaultClient,
 		apiKey:         "1234567890",
@@ -49,16 +54,16 @@ func TestFetchTournaments(t *testing.T) {
 		testName      string
 		mockDate      string
 		mockFetchData FetchData
-		wantData      map[int]string
+		wantData      map[string]string
 		wantErr       error
 	}{
 		{
 			testName:      "response ok one tournament no pagination",
 			mockDate:      time.Now().Local().Format("2006-01-02"),
-			mockFetchData: New(server.URL, "asdfghjkl", http.DefaultClient, 20),
-			wantData: map[int]string{
-				1: "test",
-				2: "test2",
+			mockFetchData: New(server.URL, "mock api key", http.DefaultClient, 5*time.Second),
+			wantData: map[string]string{
+				"1": "test",
+				"2": "test2",
 			},
 			wantErr: nil,
 		},
@@ -78,4 +83,67 @@ func TestFetchTournaments(t *testing.T) {
 			}
 		})
 	}
+}
+
+// mockFunctions
+func mockFetchTournamentEndpoint(w http.ResponseWriter, r *http.Request) {
+	apiKey := r.Header.Get("Authorization")
+	if !testApiKeyAuth(apiKey) {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+
+	date := r.URL.Query().Get("created_after")
+	if date == "2022-07-16" {
+		w.Write([]byte("[]"))
+		return
+	}
+
+	mockReturnValue := `
+	{
+		"data": [
+			{
+				"id": "1",
+				"type": "type",
+				"attributes": {
+					"tournament_type": "tournament_type",
+					"name": "testName",
+					"state": "state",
+					"game_name": "test"
+				}
+			},
+			{
+				"id": "2",
+				"type": "type",
+				"attributes": {
+					"tournament_type": "tournament_type",
+					"name": "testName",
+					"state": "state",
+					"game_name": "test2"
+				}
+			}
+		],
+		"included": [
+			{
+				"id": "blazblue-central-fiction",
+				"type": "game",
+				"attributes": {
+					"name": "BlazBlue: Central Fiction",
+					"aliases": [
+						"blazeblue central fiction"
+					],
+					"verified": true
+				}
+			}
+		]
+	}
+	`
+	byteValue := []byte(mockReturnValue)
+	w.Write(byteValue)
+}
+
+func testApiKeyAuth(apiKey string) bool {
+	return apiKey == MOCK_API_KEY
 }
