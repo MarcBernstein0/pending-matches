1c71399c673c8a5f188c6890dfb3b3731b6d7016
Marc Bernstein <marcbernstein98@gmail.com>
2023-11-30 00:19:43 -0500

    add route for healthcheck

--- main.go
+++ main.go
@@ -1,42 +1,51 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 
 	challongebracketmatches "github.com/MarcBernstein0/pending-matches/challonge-bracket-matches"
+	"github.com/MarcBernstein0/pending-matches/challonge-bracket-matches/cache"
+	"github.com/MarcBernstein0/pending-matches/route"
 )
 
 func main() {
-	// r := chi.NewRouter()
-	// r.Use(middleware.Logger)
-	// r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	w.Write([]byte("Hello World!"))
-	// })
-	// log.Fatal(http.ListenAndServe(":8080", r))
-
-	// port, present := os.LookupEnv("PORT")
-	// if !present {
-	// 	port = "8080"
-	// }
+	slog.Info("Test slog")
+	port, present := os.LookupEnv("PORT")
+	if !present {
+		port = "8080"
+	}
 
 	apiKey, present := os.LookupEnv("API_KEY")
 	if !present {
 		log.Fatalf("api_key not provided in env")
 	}
+	cacheTimerString, present := os.LookupEnv("CACHE_TIMER")
+	if !present {
+		cacheTimerString = "3"
+	}
+	cacheTimer, err := strconv.Atoi(cacheTimerString)
+	if err != nil {
+		log.Fatalf("cacheTimer could not be read properly\n%s", err)
+	}
+
+	cacheLastClearTimerString, present := os.LookupEnv("CACHE_CLEAR_TIMER")
+	if !present {
+		cacheLastClearTimerString = "5"
+	}
+	cacheClearTimer, err := strconv.Atoi(cacheLastClearTimerString)
+	if err != nil {
+		log.Fatalf("cacheTimer could not be read properly\n%s", err)
+	}
 
 	customClient := challongebracketmatches.New("https://api.challonge.com/v2.1", apiKey, http.DefaultClient, 20*time.Minute)
-	apiRes, _ := customClient.FetchTournaments(context.Background(), "2023-11-25")
-	fmt.Println(apiRes)
-	for key, val := range apiRes {
-		fmt.Println(val)
-		apiParRes, _ := customClient.FetchParticipants(context.Background(), key, val)
-		apiMatchRes, _ := customClient.FetchMatches(context.Background(), apiParRes)
-		fmt.Printf("%+v\n", apiMatchRes)
-		break
-	}
+	customCache := cache.NewCache(time.Duration(cacheTimer)*time.Minute, time.Duration(cacheClearTimer)*time.Hour)
+
+	r := route.RouterSetup(customClient, customCache)
+
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
---
+++ route/router-setup.go
@@ -0,0 +1,30 @@
+package route
+
+import (
+	"net/http"
+
+	challongebracketmatches "github.com/MarcBernstein0/pending-matches/challonge-bracket-matches"
+	"github.com/MarcBernstein0/pending-matches/challonge-bracket-matches/cache"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func RouterSetup(fetchData challongebracketmatches.FetchData, cache *cache.Cache) *chi.Mux {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+
+	r.Route("/api/v1", func(r chi.Router) {
+		r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(`
+			{
+				"status": "UP"
+			}
+			`))
+		})
+	})
+
+	return r
+}
